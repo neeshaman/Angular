a86a240df84d596e5080d7f34db2872c93427c88
neesha mansuri <201912066@daiict.ac.in>
2021-08-06 17:38:14 +0530

    NgForms

--- td-form/src/app/app.component.ts
+++ td-form/src/app/app.component.ts
@@ -8,14 +8,50 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('form') signupForm!:NgForm;
-  suggestUserName() {
+  defaultQuestion ="pet";
+  answer='';
+  genders = ['male','female']; 
+  user = {
+    username:'',
+    email:'',
+    secretQuestion:'',
+    answer:'',
+    gender:''
+  }
+  submitted = false;
+  suggestUsername() {
     const suggestedName = 'Superuser';
+    //setValue to set the whole form
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email:''
+    //   },
+    //   secret:'pet',
+    //   questionAnswer:'',
+    //   gender:'female'
+    // });
+    //patch value to override parts of the forms
+    this.signupForm.form.patchValue({
+        userData:{
+          username:suggestedName
+        }
+    });
   }
 
   // onSubmit(form:NgForm){
   //   console.log(form);
   // }
   onSubmit(){
+    this.submitted =true;
     console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+
+    this.signupForm.reset();
   }
 }
