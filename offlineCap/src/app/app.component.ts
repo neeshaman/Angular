73d960b264fb76cfe7be0948bfb05465419a121e
neesha mansuri <201912066@daiict.ac.in>
2021-08-08 20:24:45 +0530

    Made offline capability with service worker

---
+++ offlineCap/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Post } from './post.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  posts: Post[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+      .subscribe((fetchedPosts: Post[]) => (this.posts = fetchedPosts));
+  }
+}
